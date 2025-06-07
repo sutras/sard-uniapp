c9e097088bebd5259223451dc6cca086ef2dfb70
wuzhitao <joinpass@outlook.com>
2025-06-08 00:02:08 +0800

    docs(demo): 密码输入框添加切换按钮

--- src/pages/components/input/demo/Type.vue
+++ src/pages/components/input/demo/Type.vue
@@ -4,5 +4,16 @@
   <sar-input placeholder="idcard" type="idcard" />
   <sar-input placeholder="digit" type="digit" />
   <sar-input placeholder="tel" type="tel" />
-  <sar-input placeholder="password" type="password" />
+  <sar-input placeholder="password" :type="isPlainText ? 'text' : 'password'">
+    <template #append>
+      <view @click="isPlainText = !isPlainText">
+        {{ isPlainText ? '显示密文' : '显示明文' }}
+      </view>
+    </template>
+  </sar-input>
 </template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+const isPlainText = ref(false)
+</script>
