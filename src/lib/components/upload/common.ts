b4544ce6ce19f76a3aff79f78e7a171fb2086062
⺗涛 <1425093691@qq.com>
2025-06-20 17:56:22 +0800

    upload组件修改beforeChoose，多返回一个参数sourceType，用于区分当前选择项。 优化了在小程序端选择文件使用的是uni.chooseMedia()

--- src/lib/components/upload/common.ts
+++ src/lib/components/upload/common.ts
@@ -4,90 +4,91 @@ import { defaultConfig } from '../config'
 export type UploadStatus = 'pending' | 'uploading' | 'failed' | 'done'
 
 export interface UploadFile {
-  type: 'image' | 'video'
-  size: number
-  path: string
-  duration: number
-  width: number
-  height: number
+	type : 'image' | 'video'
+	size : number
+	path : string
+	duration : number
+	width : number
+	height : number
 }
 
 export interface UploadFileItem {
-  file?: UploadFile
-  url?: string
-  isImage?: boolean
-  isVideo?: boolean
-  status?: UploadStatus
-  name?: string
-  message?: string
+	file ?: UploadFile
+	url ?: string
+	isImage ?: boolean
+	isVideo ?: boolean
+	status ?: UploadStatus
+	name ?: string
+	message ?: string
 }
 
 export interface UploadProps {
-  rootStyle?: StyleValue
-  rootClass?: string
+	rootStyle ?: StyleValue
+	rootClass ?: string
 
-  accept?: 'image' | 'video'
-  multiple?: boolean
-  sourceType?: ('album' | 'camera')[]
-  sizeType?: ('original' | 'compressed')[]
-  maxDuration?: number
-  camera?: 'back' | 'front'
-  modelValue?: UploadFileItem[]
-  maxCount?: number
-  maxSize?: number | ((file: UploadFile) => boolean)
-  overSize?: (fileItem: UploadFileItem[]) => void
-  disabled?: boolean
-  readonly?: boolean
-  beforeChoose?: (
-    fileList: UploadFileItem[],
-    next: (allowed: boolean) => void,
-  ) => void
-  beforeRead?: (file: UploadFile) => boolean | Promise<UploadFile>
-  afterRead?: (fileItem: UploadFileItem) => void
-  removable?: boolean
-  beforeRemove?: (...args: unknown[]) => boolean | Promise<void>
-  validateEvent?: boolean
+	accept ?: 'image' | 'video'
+	multiple ?: boolean
+	sourceType ?: ('album' | 'camera')[]
+	sizeType ?: ('original' | 'compressed')[]
+	maxDuration ?: number
+	camera ?: 'back' | 'front'
+	modelValue ?: UploadFileItem[]
+	maxCount ?: number
+	maxSize ?: number | ((file : UploadFile) => boolean)
+	overSize ?: (fileItem : UploadFileItem[]) => void
+	disabled ?: boolean
+	readonly ?: boolean
+	beforeChoose ?: (
+		fileList : UploadFileItem[],
+		sourceType : ['album'] | ['camera'],
+		next : (allowed : boolean) => void,
+	) => void
+	beforeRead ?: (file : UploadFile) => boolean | Promise<UploadFile>
+	afterRead ?: (fileItem : UploadFileItem) => void
+	removable ?: boolean
+	beforeRemove ?: (...args : unknown[]) => boolean | Promise<void>
+	validateEvent ?: boolean
 }
 
 export const defaultUploadProps = defaultConfig.upload as Omit<
-  typeof defaultConfig.upload,
-  'sourceType' | 'sizeType'
+	typeof defaultConfig.upload,
+	'sourceType' | 'sizeType'
 >
 
 export interface UploadSlots {
-  select?(props: Record<string, never>): any
+	select ?(props : Record<string, never>) : any
 }
 
 export interface UploadEmits {
-  (e: 'update:model-value', value: UploadFileItem[]): void
-  (e: 'change', value: UploadFileItem[]): void
-  (e: 'remove', index: number, item: UploadFileItem): void
+	(e : 'update:model-value', value : UploadFileItem[]) : void
+	(e : 'change', value : UploadFileItem[]) : void
+	(e : 'remove', index : number, item : UploadFileItem) : void
 }
 
 export interface ChainNode {
-  (data: any, next: (...args: any[]) => void): void
+	(data : any, next : (...args : any[]) => void) : void
 }
 
 export interface UploadPreviewProps {
-  rootStyle?: StyleValue
-  rootClass?: string
-  file?: UploadFile
-  url?: string
-  isImage?: boolean
-  isVideo?: boolean
-  status?: UploadStatus
-  name?: string
-  message?: string
-  removable?: boolean
-  beforeRemove?: (index: number) => boolean | Promise<void>
-  index: number
-  disabled?: boolean
-  readonly?: boolean
+	rootStyle ?: StyleValue
+	rootClass ?: string
+	file ?: UploadFile
+	url ?: string
+	isImage ?: boolean
+	isVideo ?: boolean
+	status ?: UploadStatus
+	name ?: string
+	message ?: string
+	removable ?: boolean
+	beforeRemove ?: (index : number) => boolean | Promise<void>
+	index : number
+	disabled ?: boolean
+	readonly ?: boolean
 }
 
 export const defaultUploadPreviewProps = defaultConfig.uploadPreview
 
 export interface UploadPreviewEmits {
-  (e: 'image-click', index: number): void
-  (e: 'remove', index: number): void
+	(e : 'image-click', index : number) : void
+	(e : 'remove', index : number) : void
 }
